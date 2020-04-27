626e600abe98c2f570689134ded71dc0a1a2190c
Fellip <43684955+fellipmelo@users.noreply.github.com>
2020-04-26 20:08:48 -0400

    Desinstalar programa

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+rm -r /etc/tunnelngrok
+rm /bin/tunnelngrok.py
+rm /usr/bin/tunnelngrok.py
+rm ngrok
+rm ngrok-stable-linux-amd64.zip
+chmod 666 ngroktunnel.py  README.md 
+rm /bin/ngrok
+rm -r /root/.ngrok2
+rm /bin/tn.py
